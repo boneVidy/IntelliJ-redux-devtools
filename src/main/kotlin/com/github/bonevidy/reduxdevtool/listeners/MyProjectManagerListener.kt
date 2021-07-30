7942c08b34d61add83bf547f2eed02940d389a81
GitHub Action <action@github.com>
2021-07-30 02:02:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/bonevidy/reduxdevtool/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/bonevidy/reduxdevtool/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bonevidy.reduxdevtool.listeners
 
+import com.github.bonevidy.reduxdevtool.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/bonevidy/reduxdevtool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bonevidy/reduxdevtool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bonevidy.reduxdevtool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bonevidy.reduxdevtool.MyBundle
 
 class MyApplicationService {
 
